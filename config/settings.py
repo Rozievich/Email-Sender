c96a169456982818c825dd1de88c1b09d1a7eddf
Rozievich <blackcoder459@outlook.com>
2023-12-19 14:59:59 +0500

    Update for code

--- config/settings.py
+++ config/settings.py
@@ -127,8 +127,8 @@ CACHES = {
 }
 
 
-EMAIL_HOST_USER = 'oybekn033@gmail.com'
-EMAIL_HOST_PASSWORD = 'lxqqztmlarzwexjm'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your email password'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
